9baedd83256c529295592916b93e6a767fd96f21
Kevin Gallas <kevingallas@Kevins-MBP.home>
2022-07-13 14:59:24 +0200

    initial commit

--- lib/hello-ecs-stack.ts
+++ lib/hello-ecs-stack.ts
@@ -1,16 +1,41 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
+import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
-export class HelloEcsStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
+import * as ecs from 'aws-cdk-lib/aws-ecs';
+import * as ecsp from 'aws-cdk-lib/aws-ecs-patterns';
+
+import * as ecr_assets from 'aws-cdk-lib/aws-ecr-assets'
+import * as secretsmanager from 'aws-cdk-lib/aws-secretsmanager';
+import * as fs from 'fs';
+
+export class HelloEcsStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const secretKeysFromFile = fs.readFileSync(`./env.secret_keys`, 'utf8')
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'HelloEcsQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    let listOfSecrets: string[] = []
+    secretKeysFromFile.split(/\r?\n/).forEach(line => {
+      listOfSecrets.push(line)
+    });
+    let ecsSecrets: { [key: string]: ecs.Secret } = {}
+    listOfSecrets.forEach(secret => {
+      let ecsSecret: secretsmanager.ISecret = secretsmanager.Secret.fromSecretNameV2(
+        this, 
+        `my-example-secret`,
+        'this-is-an-example-secret')
+      ecsSecrets[secret] = ecs.Secret.fromSecretsManager(ecsSecret, secret);
+    })
+
+    new ecsp.ApplicationLoadBalancedFargateService(this, 'MyWebServer', {
+      taskImageOptions: {
+        secrets: ecsSecrets,
+        // image: ecs.ContainerImage.fromRegistry('amazon/amazon-ecs-sample'),
+        image: ecs.ContainerImage.fromDockerImageAsset(new ecr_assets.DockerImageAsset(this, 'MyContainerImageAsset', {
+          directory: './'
+        }),
+      )},
+      publicLoadBalancer: true
+    });
   }
 }
